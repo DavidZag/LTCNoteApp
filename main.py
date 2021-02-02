1e7af10800abd4bf70c60c679d0fc330239fec56
davidzag <davidzag@berkeley.edu>
2021-02-01 20:29:07 -0800

    did cool thing

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-import ltc_reader
-ltc_reader.start_read_ltc()
+#import ltc_reader
+#ltc_reader.start_read_ltc()
+from datetime import datetime
+
+#Startup Application
+f = open("notes.txt", "a")
+notes = ""
+
+#Running 
 
 while True:
-  takeNotes()
-  getBitCoin()
+  n = input()
+
+  if str(n) == "done":
+    break
+
+  now = datetime.now()
+  n = n + " | " + str(now) + "\n"
+  notes += n
+  print(n)
+
+
+#Finished
+
+f.write(notes)
